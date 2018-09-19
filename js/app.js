7eea2aaedde0c1ca47d76f34d55cb2d37f8f63db
Andrzej Ruga <andrzej@ruga.pl>
2018-09-19 11:35:39 +0200

    Added events for slider buttons in app.js file

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// slider
+
+var btnPrev = document.querySelector('.banner-prev');
+var btnNext = document.querySelector('.banner-next');
+var banners = document.querySelectorAll('.banner-slide');
+
+var bannerIndex = 0;
+btnNext.addEventListener('click', function () {
+    banners[bannerIndex].classList.remove('banner-slide-active');
+    bannerIndex++;
+    if (bannerIndex > (banners.length - 1)) {
+        bannerIndex = 0;
+        banners[bannerIndex].classList.add('banner-slide-active')
+    } else {
+        banners[bannerIndex].classList.add('banner-slide-active')
+    }
+});
+
+btnPrev.addEventListener('click', function () {
+    banners[bannerIndex].classList.remove('banner-slide-active');
+    bannerIndex--;
+    if (bannerIndex < 0) {
+        bannerIndex = (banners.length - 1);
+        banners[bannerIndex].classList.add('banner-slide-active')
+    } else {
+        banners[bannerIndex].classList.add('banner-slide-active')
+    }
+});
+
+// the above way is more universal, it works with more than just 2 banners
+// for just 2 and no more, it could look much easier
+//
+// btnNext.addEventListener('click', function () {
+//     banners.forEach(item => {
+//         item.classList.contains('banner-slide-active') ?
+//             item.classList.remove('banner-slide-active') :
+//             item.classList.add('banner-slide-active')
+//     })
+// });
+// btnPrev.addEventListener('click', function () {
+//     banners.forEach(item => {
+//         item.classList.contains('banner-slide-active') ?
+//             item.classList.remove('banner-slide-active') :
+//             item.classList.add('banner-slide-active')
+//     })
+// });
+
+
+// article mouseover
