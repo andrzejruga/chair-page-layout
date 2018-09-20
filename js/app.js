115b6b1b065075fab2740169c7cf77d3e16bd9be
Andrzej Ruga <andrzej@ruga.pl>
2018-09-20 20:38:36 +0200

    Added mouseover effect to articles' links

--- js/app.js
+++ js/app.js
@@ -46,5 +46,34 @@ btnPrev.addEventListener('click', function () {
 // });
 
 
-// article mouseover
+// article mouseover effect
+
+var articleLinks = document.querySelectorAll('.main-article-link');
+
+// for (var i=0; i<articleLinks.length; i++) {
+//     articleLinks[i].addEventListener('mouseover', function () {
+//         var articleBar = this.querySelector('.main-article-bar');
+//         articleBar.style.display = 'none';
+//     })
+// }
+// for (var i=0; i<articleLinks.length; i++) {
+//     articleLinks[i].addEventListener('mouseout', function () {
+//         var articleBar = this.querySelector('.main-article-bar');
+//         articleBar.style.display = 'block';
+//     })
+// }
+// *** but code below looks IMO a bit cleaner
+
+articleLinks.forEach(item => {
+    item.addEventListener('mouseover', function () {
+        var articleBar = this.querySelector('.main-article-bar');
+        articleBar.style.display = 'none';
+    })
+});
+articleLinks.forEach(item => {
+    item.addEventListener('mouseout', function () {
+        var articleBar = this.querySelector('.main-article-bar');
+        articleBar.style.display = 'block';
+    })
+});
 
